f355c043015d64b3a781701628fd8afac6214f14
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-02-27 22:15:03 +0300

    added name to the search bar in the index.html form, extracted the variable from the url and passed the variable to the filter function in the home view. Imported the Q from db models and used for displaying the value with the same match

--- photos/views.py
+++ photos/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .models import Photo, Category
-
+from django.db.models import Q
 # Create your views here.
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
+    # t = request.GET.get('t') if request.GET.get('t') !=  None else ''
     print(q)
-    photos = Photo.objects.filter(category__name__icontains = q)
+    photos = Photo.objects.filter(Q(category__name__icontains = q) |
+                                    Q(location__name__icontains = q))
     category = Category.objects.all()
     context = {'photos': photos, 'category':category}
     return render(request, 'photos/index.html', context)
