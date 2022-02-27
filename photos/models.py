851a663e01ee391b94955fe769525b935c7f938c
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-02-27 11:42:57 +0300

    created location, category and Photo models and their respective string representation

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Photo(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.description
