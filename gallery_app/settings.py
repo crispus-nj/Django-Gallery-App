dcf3a9c6c77208e663075adb50e8a86a6dcbeb7c
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-02-28 08:47:38 +0300

    added staticfiles configuration to the setting.py and custome styling to the navigation

--- gallery_app/settings.py
+++ gallery_app/settings.py
@@ -123,9 +123,12 @@ MEDIA_URL = '/images/'
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
