e71dc9ddc9b6970d5a45df599d3985cdb58ddfa3
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-01 12:03:31 +0300

    created docstrings to all tests functions. Added test to test if the objects are delete successfully

--- photos/tests.py
+++ photos/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+
+from photos.views import photo
 from .models import Category, Photo, Location
 
 class CategoryTestClass(TestCase):
@@ -26,14 +28,25 @@ class PhotoTestClass(TestCase):
         '''
         self.category = Category.objects.create(name = 'Pet')
         self.location = Location.objects.create(name = 'Thika')
-        self.crispus = Photo(category = self.category, location = self.location, image ='test.jpg', description="Nice pet")
+        self.crispus = Photo(id = 1, category = self.category, location = self.location, image ='test.jpg', description="Nice pet")
         
 
     def test_save(self):
+        '''
+        test-save function is a function for testing to if objects are added successfully to the database or not!
+        '''
         self.crispus.save()
         photos = Photo.objects.all()
         self.assertTrue(len(photos) > 0)
-        
+
+    def test_delete(self):
+        '''
+        test_delete is a function for testing is objects are deleted successfully from the database
+        '''
+        self.crispus.delete_image()
+        photos = Photo.objects.all()
+        self.assertEqual(len(photos), 0)
+
     # Testing  instance
     def test_instance(self):
         self.assertTrue(isinstance(self.crispus, Photo))
